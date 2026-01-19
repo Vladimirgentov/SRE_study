1f50329fabca553cffd1e85405b25846a94ac5e0
Vladimirgentov <v@gentov.ru>
2026-01-19 03:35:20 +0300

    Add docker compose production, backend Dockerfile, and finalize nginx/static config

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -56,9 +56,13 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
