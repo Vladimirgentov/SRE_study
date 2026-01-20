7b33d8e84967d6571e9d54ca1571a9b91a0b488b
Vladimirgentov <v@gentov.ru>
2026-01-21 01:31:53 +0300

    Fix env settings defaults and nginx static/media routing

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,15 +1,40 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = True
+def str_to_bool(value: str) -> bool:
+    return value.strip().lower() in ("1", "true", "yes", "y", "on")
 
-ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = "/app/collected_static"
+def parse_allowed_hosts(value: str) -> list[str]:
+    """
+    Поддерживаем:
+    - "*" (разрешить всё)
+    - "example.com,localhost,127.0.0.1"
+    - "example.com localhost 127.0.0.1"
+    """
+    v = (value or "").strip()
+    if not v:
+        return []
+    if v == "*":
+        return ["*"]
+
+
+    v = v.replace(" ", ",")
+    hosts = [h.strip() for h in v.split(",") if h.strip()]
+    return hosts
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
+
+
+DEBUG = str_to_bool(os.getenv("DEBUG", "False"))
+
+ALLOWED_HOSTS = parse_allowed_hosts(os.getenv("ALLOWED_HOSTS", "*"))
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -18,9 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework.authtoken',
     'rest_framework',
     'djoser',
+
     'cats.apps.CatsConfig',
 ]
 
@@ -58,62 +85,46 @@ WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("POSTGRES_DB", "kittygram"),
+        "USER": os.getenv("POSTGRES_USER", "kittygram_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "kittygram_password"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static') 
+STATIC_ROOT = "/app/collected_static"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/app/media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
